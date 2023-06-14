107951dc5c92b31fd2fa4bd4b5e8887d7d385c18
Yanqiao4396 <chen02@allegheny.edu>
2023-06-13 21:34:58 -0400

    Feat: add exception statement for update and create file
    tried to update file, if fails to find the file, then create that file.

--- main.py
+++ main.py
@@ -15,41 +15,54 @@ OWNER = "allegheny-college-sandbox"
 AUTHENTICATED_API = github.Github("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")
 
 
-
 class insightBranch:
     """insight branch."""
-    def __init__(self,branch_name = "insight") -> None:
+
+    def __init__(self, branch_name="insight") -> None:
         """initialize an insight branch if it doesn't exist yet."""
         self.repo_obj = AUTHENTICATED_API.get_organization(OWNER).get_repo(CUR_REPO)
         self.insight_branch = branch_name
-        self.ref = ref='refs/heads/' + branch_name
+        self.ref = ref = "refs/heads/" + branch_name
+        self.default_branch = (
+            AUTHENTICATED_API.get_organization(OWNER).get_repo(CUR_REPO).default_branch
+        )
+        self.default_branch_obj = self.repo_obj.get_branch(self.default_branch)
 
         # Check if insight branch exists or not.
         found_insight_branch = False
         for branch in self.repo_obj.get_branches():
             if branch.name == branch_name:
                 found_insight_branch = True
-        
+
         # Create insight branch if not.
         if not found_insight_branch:
-
-            default_branch_name = self.repo_obj.default_branch
-            default_branch_obj = self.repo_obj.get_branch(default_branch_name)
+            default_branch_obj = self.repo_obj.get_branch(self.default_branch)
             self.repo_obj.create_git_ref(self.ref, sha=default_branch_obj.commit.sha)
 
-    
-    def content_upload(self,content, path = "."):
+    def content_upload(self, user_content, path="."):
         """Upload the content to a specific path in the insight branch."""
 
         # If fails to find the path, create one.
-        try: 
-            file = self.repo_obj.get_contents(path,ref = self.insight_branch)
-            print(file.content)
+        try:
+            file = self.repo_obj.get_contents(path, ref=self.insight_branch)
+            self.repo_obj.update_file(
+                path=path,
+                message="insight upload",
+                content=user_content,
+                branch=self.insight_branch,
+                sha=file.sha,
+            )
+            print("🚀 successfully updated a file!")
         except github.GithubException:
-            self.repo_obj.create_file(path=path, message="insight upload",content="insight is cool",branch=self.insight_branch)
-        
+            self.repo_obj.create_file(
+                path=path,
+                message="insight upload",
+                content=user_content,
+                branch=self.insight_branch,
+            )
+            print("🚀 successfully created a file!")
 
 
 if __name__ == "__main__":
     branch = insightBranch()
-    branch.content_upload(content= "hello", path="./cool/insight.json")
+    branch.content_upload(user_content="hello", path="./cool/insight.json")
