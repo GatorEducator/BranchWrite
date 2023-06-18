43a21ac2bf904380ffd2477a129ad6267127d6e2
Yanqiao4396 <chen02@allegheny.edu>
2023-06-18 16:41:44 -0400

    Factor: add more comments

--- branchwrite.py
+++ branchwrite.py
@@ -13,25 +13,27 @@ AUTHENTICATED_API = github.Github(TOKEN)
 
 
 class BranchWrite:
-    """insight branch."""
+    """target branch."""
 
     def __init__(self, branch_name="insight", des_path = ".") -> None:
-        """initialize an insight branch if it doesn't exist yet."""
+        """initialize the wanted branch if it doesn't exist yet."""
         self.repo_obj = AUTHENTICATED_API.get_repo(CUR_REPO)
         self.insight_branch = branch_name
         self.ref = "refs/heads/" + branch_name
         self.des_path = des_path
+
+        # Get default branch for the use of branch sha
         self.default_branch = (
             AUTHENTICATED_API.get_repo(CUR_REPO).default_branch
         )
-
         self.default_branch_obj = self.repo_obj.get_branch(self.default_branch)
-        # Check if insight branch exists or not.
+
+        # Check if the wanted branch exists or not.
         found_insight_branch = False
         for branch in self.repo_obj.get_branches():
             if branch.name == branch_name:
                 found_insight_branch = True
-        # Create insight branch if not.
+        # Create the wanted branch if not.
         if not found_insight_branch:
             default_branch_obj = self.repo_obj.get_branch(self.default_branch)
             self.repo_obj.create_git_ref(self.ref, sha=default_branch_obj.commit.sha)
@@ -39,12 +41,14 @@ class BranchWrite:
     def from_string(self,content):
         """Upload the content to a specific path in the insight branch."""
         self.__write_content(content)
+        return
 
     def from_file(self, source_branch, source_file):
         """Upload content to a specific path in the insight branch from a file in another branch."""
         file = self.repo_obj.get_contents(source_file, ref=source_branch)
         content = base64.b64decode(file.content)
         self.__write_content(content)
+        return
 
     def from_env(self, env):
         """Upload content to a specific path in the insight branch from a environment variable."""
@@ -52,8 +56,10 @@ class BranchWrite:
         if not content:
             raise ValueError(f"Can't find {env}, exit the program.")
         self.__write_content(content)
+        return
 
     def __write_content(self,content):
+        """Write a file in the destination insight with the given content."""
         # If fails to find the path, create one.
         try:
             file = self.repo_obj.get_contents(self.des_path, ref=self.insight_branch)
@@ -76,20 +82,23 @@ class BranchWrite:
 def main():
     args = sys.argv
     branch_name = args[1]
-    target_branch = BranchWrite(branch_name)
     target_path = args[2]
+    target_branch = BranchWrite(branch_name, target_path)
+
 
     # args[3] decides which function to use and has three options: content, branch-and-file, env
     source = args[3]
     if source == "content":
         content = args[4]
-        target_branch.content_upload_from_string(target_path,content)
+        target_branch.from_string(content)
+
     elif source == "branch-and-path":
         source_branch, source_path = args[4].split("/")[0], "/".join(args[4].split("/")[1:])
-        target_branch.content_upload_from_file(target_path,source_branch,source_path)
+        target_branch.from_file(source_branch,source_path)
     
     else:
-        print("env will come in the future")
+        env = args[4]
+        target_branch.from_env(env)
         
 
         
